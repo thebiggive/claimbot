c0538ff4a63486b6f2dc0e3d4d8e9f6b12475f89
Noel Light-Hilary <noel@noellh.com>
2021-12-28 09:50:19 +0000

    CLA-3 - add first unit test

---
+++ tests/Exception/DonationDataErrorsExceptionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ClaimBot\Tests\Exception;
+
+use ClaimBot\Exception\DonationDataErrorsException;
+use PHPUnit\Framework\TestCase;
+
+class DonationDataErrorsExceptionTest extends TestCase
+{
+    private DonationDataErrorsException $exception;
+
+    public function setUp(): void
+    {
+        $fullErrors = [
+            [
+                'message' => "Invalid content found at element 'Sur'",
+                'location' => '/hd:GovTalkMessage[1]/hd:Body[1]/r68:IRenvelope[1]/r68:R68[1]/' .
+                    'r68:Claim[1]/r68:Repayment[1]/r68:GAD[1]/r68:Donor[1]/r68:Sur[1]',
+            ],
+            [
+                'message' => "Invalid content found at element 'Fore'",
+                'location' => '/hd:GovTalkMessage[1]/hd:Body[1]/r68:IRenvelope[1]/r68:R68[1]/' .
+                    'r68:Claim[2]/r68:Repayment[1]/r68:GAD[1]/r68:Donor[1]/r68:Sur[1]',
+            ],
+        ];
+
+        $errorMessages = print_r([
+            "Invalid content found at element 'Sur'",
+            "Invalid content found at element 'Fore'",
+        ], true);
+
+        $this->exception = new DonationDataErrorsException($fullErrors, $errorMessages);
+    }
+
+    public function testErrorListGetter()
+    {
+        $this->assertCount(2, $this->exception->getDonationErrors());
+        $this->assertEquals(
+            "Invalid content found at element 'Sur'",
+            $this->exception->getDonationErrors()[0]['message'],
+        );
+
+        $this->assertEquals(<<<EOT
+Array
+(
+    [0] => Invalid content found at element 'Sur'
+    [1] => Invalid content found at element 'Fore'
+)
+
+EOT, $this->exception->getMessage());
+    }
+}
