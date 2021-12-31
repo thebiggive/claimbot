1dd6833d0da0afe8e3a9655d9da3264a8a5e8d50
Noel Light-Hilary <noel@noellh.com>
2021-12-31 11:55:24 +0000

    CLA-7 - test batch logging

--- tests/Monolog/Handler/ClaimBotHandlerTest.php
+++ tests/Monolog/Handler/ClaimBotHandlerTest.php
@@ -12,7 +12,9 @@ use Monolog\Logger;
 
 class ClaimBotHandlerTest extends TestCase
 {
-    public function testHandler(): void
+    private ClaimBotHandler $handler;
+
+    public function setUp(): void
     {
         $container = $this->getContainer();
         $container->set(
@@ -20,7 +22,7 @@ class ClaimBotHandlerTest extends TestCase
             $this->prophesize(CloudWatchLogsClient::class)->reveal(),
         );
 
-        $handler = new ClaimBotHandler(
+        $this->handler = new ClaimBotHandler(
             $container->get(CloudWatchLogsClient::class),
             [
                 'name' => 'claimbot',
@@ -34,7 +36,10 @@ class ClaimBotHandlerTest extends TestCase
             ],
             'test',
         );
+    }
 
+    public function testSingleHandles(): void
+    {
         /**
          * We set 'bubble' to true, because we need all handlers in the group to see each message, since the
          * 'routing' of messages is based on context and not level. This means `GroupHandler` will always return
@@ -42,7 +47,7 @@ class ClaimBotHandlerTest extends TestCase
          * with the log, so that logs are still provided to the rest of the handler group.
          */
         $this->assertFalse(
-            $handler->handle([
+            $this->handler->handle([
                 'message' => 'Some standard log error message',
                 'level' => Logger::ERROR,
                 'context' => [],
@@ -52,7 +57,7 @@ class ClaimBotHandlerTest extends TestCase
         );
 
         $this->assertFalse(
-            $handler->handle([
+            $this->handler->handle([
                 'message' => 'Some Gift Aid request data',
                 'level' => Logger::INFO,
                 'context' => ['gift_aid_message' => 'request'],
@@ -62,7 +67,7 @@ class ClaimBotHandlerTest extends TestCase
         );
 
         $this->assertFalse(
-            $handler->handle([
+            $this->handler->handle([
                 'message' => 'Some Gift Aid response data',
                 'level' => Logger::INFO,
                 'context' => ['gift_aid_message' => 'response'],
@@ -70,5 +75,35 @@ class ClaimBotHandlerTest extends TestCase
                 'datetime' => new DateTimeImmutable(true),
             ]),
         );
+    }
+
+    public function testBatchHandle(): void
+    {
+        $this->handler->handleBatch([
+            [
+                'message' => 'Some standard log error message',
+                'level' => Logger::ERROR,
+                'context' => [],
+                'extra' => [],
+                'datetime' => new DateTimeImmutable(true),
+            ],
+            [
+                'message' => 'Some Gift Aid request data',
+                'level' => Logger::INFO,
+                'context' => ['gift_aid_message' => 'request'],
+                'extra' => [],
+                'datetime' => new DateTimeImmutable(true),
+            ],
+            [
+                'message' => 'Some Gift Aid response data',
+                'level' => Logger::INFO,
+                'context' => ['gift_aid_message' => 'response'],
+                'extra' => [],
+                'datetime' => new DateTimeImmutable(true),
+            ],
+        ]);
+
+        // Simply sanity check that a batch log call doesn't crash.
+        $this->addToAssertionCount(1);
     }
 }
